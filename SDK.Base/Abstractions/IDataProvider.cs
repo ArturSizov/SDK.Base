ad3634cbcaf0f1d8151c8f729595ec5e66aef2f7
ArturSizov <Artur.Sizov@outlook.com>
2024-07-17 21:25:28 +0300

    Added initial resources

---
+++ SDK.Base/Abstractions/IDataProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace SDK.Base.Abstractions
+{
+    /// <summary>
+    /// Data provider interface
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IDataProvider<T> : IBasicCRUD<T>
+    {
+    }
+}
